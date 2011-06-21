4fd652d9a93045bb63f1ad53c548077e8513b8c4
Raoul Millais <raoul@raoulmillais.com>
2011-06-21 13:28:48 +0100

    Added app.js to bootstrap wheat

---
+++ app.js
@@ -0,0 +1,12 @@
+var Connect = require('connect');
+
+module.exports = Connect.createServer(
+  Connect.logger(),
+  Connect.conditionalGet(),
+  Connect.favicon(),
+  Connect.cache(),
+  Connect.gzip(),
+  require('wheat')(__dirname)
+);
+
+
